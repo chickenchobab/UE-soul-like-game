95d6eecc5f8ea6abf4a409f3e35fcc28c729f077
chickenchobab <hew980812@gmail.com>
2025-07-14 16:29:07 +0900

    Handle inputs by gameplay tags (move and look)

--- Source/SoulLikeGame/Public/Characters/SoulHeroCharacter.h
+++ Source/SoulLikeGame/Public/Characters/SoulHeroCharacter.h
@@ -8,6 +8,8 @@
 
 class USpringArmComponent;
 class UCameraComponent;
+class UDataAsset_InputConfig;
+struct FInputActionValue;
 
 /**
  * 
@@ -21,6 +23,7 @@ public:
 	ASoulHeroCharacter();
 
 protected:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	virtual void BeginPlay() override;
 
 private:
@@ -35,5 +38,14 @@ private:
 
 #pragma endregion
 
+#pragma region Inputs
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CharacterData", meta = (AllowPrivateAccess = "true"))
+	UDataAsset_InputConfig* InputConfigDataAsset;
+
+	void Input_Move(const FInputActionValue& InputActionValue);
+	void Input_Look(const FInputActionValue& InputActionValue);
+
+#pragma endregion
 };
 
---
+++ Source/SoulLikeGame/Public/Components/Input/SoulInputComponent.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+#include "SoulInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOULLIKEGAME_API USoulInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+public:
+	template<class UserObject, typename CallbackFunc>
+	void BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func);
+};
+
+
+template<class UserObject, typename CallbackFunc>
+inline void USoulInputComponent::BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func)
+{
+	// check(InInputConfig);
+	checkf(InInputConfig, TEXT("Input config data asset is null, can not proceed with binding"))
+
+	if (UInputAction* FoundAction = InInputConfig->FindNativeInputActionByTag(InInputTag))
+	{
+		BindAction(FoundAction, TriggerEvent, ContextObject, Func);
+	}
+}
+
