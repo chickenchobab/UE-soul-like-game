e006c51bb49066b653854f313e985533076fdb12
chickenchobab <hew980812@gmail.com>
2025-08-03 15:36:36 +0900

    Handle in the combat component an enemy's hit on player

--- Source/SoulLikeGame/Private/Components/Combat/EnemyCombatComponent.cpp
+++ Source/SoulLikeGame/Private/Components/Combat/EnemyCombatComponent.cpp
@@ -2,4 +2,41 @@
 
 
 #include "Components/Combat/EnemyCombatComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "SoulGameplayTags.h"
 
+#include "SoulDebugHelper.h"
+
+void UEnemyCombatComponent::OnWeaponHitTargetActor(AActor* HitActor)
+{
+  if (OverlappedActors.Contains(HitActor)) return;
+
+  OverlappedActors.AddUnique(HitActor);
+
+  bool bIsValidBlock = false;
+
+  const bool bIsPlayerBlocking = false;
+  const bool bIsMyAttackUnblockable = false;
+
+  if (bIsPlayerBlocking && !bIsMyAttackUnblockable)
+  {
+    //TODO::Check if the block is valid
+  }
+
+  FGameplayEventData EventData;
+  EventData.Instigator = GetOwningPawn();
+  EventData.Target = HitActor;
+
+  if (bIsValidBlock)
+  {
+    //TODO::Handle successful block
+  }
+  else
+  {
+    UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+      GetOwningPawn(),
+      SoulGameplayTags::Shared_Event_MeleeHit,
+      EventData
+    );
+  }
+}
--- Source/SoulLikeGame/Public/Components/Combat/EnemyCombatComponent.h
+++ Source/SoulLikeGame/Public/Components/Combat/EnemyCombatComponent.h
@@ -14,4 +14,6 @@ class SOULLIKEGAME_API UEnemyCombatComponent : public UPawnCombatComponent
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void OnWeaponHitTargetActor(AActor* HitActor) override;
 };
