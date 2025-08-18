3cb2ef0572be43aa15b89b264fac3f57ce68cbc2
chickenchobab <hew980812@gmail.com>
2025-08-18 20:45:55 +0900

    Implement a projectile which passes through enemies

--- Source/SoulLikeGame/Private/Items/SoulProjectileBase.cpp
+++ Source/SoulLikeGame/Private/Items/SoulProjectileBase.cpp
@@ -89,7 +89,21 @@ void ASoulProjectileBase::OnProjectileHit(UPrimitiveComponent* HitComponent, AAc
 
 void ASoulProjectileBase::OnProjectileBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	
+	if (OverlappedActors.Contains(OtherActor)) return;
+
+	OverlappedActors.Add(OtherActor);
+
+	if (APawn* HitPawn = Cast<APawn>(OtherActor))
+	{
+		FGameplayEventData Data;
+		Data.Instigator = this;
+		Data.Target = HitPawn;
+
+		if (USoulFunctionLibrary::IsTargetPawnHostile(GetInstigator(), HitPawn))
+		{
+			HandleApplyProjectileDamage(HitPawn, Data);
+		}
+	}
 }
 
 void ASoulProjectileBase::HandleApplyProjectileDamage(APawn* InHitPawn, const FGameplayEventData& InPayload)
