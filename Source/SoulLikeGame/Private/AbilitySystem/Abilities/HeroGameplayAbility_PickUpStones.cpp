29af41506c894077bd550da5bfb2b89ade36ea41
chickenchobab <hew980812@gmail.com>
2025-08-25 09:28:42 +0900

    Remove typo

--- Source/SoulLikeGame/Private/AbilitySystem/Abilities/HeroGameplayAbility_PickUpStones.cpp
+++ Source/SoulLikeGame/Private/AbilitySystem/Abilities/HeroGameplayAbility_PickUpStones.cpp
@@ -16,7 +16,7 @@ void UHeroGameplayAbility_PickUpStones::ActivateAbility(const FGameplayAbilitySp
 
 void UHeroGameplayAbility_PickUpStones::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
-  GetHeroUIComponentFromActorInfo()->OnStoneInteracted.Broadcast(true);
+  GetHeroUIComponentFromActorInfo()->OnStoneInteracted.Broadcast(false);
   
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
